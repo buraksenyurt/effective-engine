29299f7d5691d92d25196602c1647aaede180571
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-14 16:03:05 +0300

    New Controller and function added.

---
+++ GalaxyExplorer/GalaxyExplorer.API/Controller/VoyagerController.cs
@@ -0,0 +1,25 @@
+﻿using GalaxyExplorer.DTO;
+using GalaxyExplorer.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GalaxyExplorer.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoyagerController : ControllerBase
+    {
+        // DI Container'a kayıtlı IMissionService uyarlaması kimse o gelecek
+        private readonly IMissionService _missionService;
+        public VoyagerController(IMissionService missionService)
+        {
+            _missionService = missionService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetVoyagers([FromQuery] GetVoyagersRequest request) // Parametreleri QueryString üzerinden almayı tercih ettim
+        {
+            var voyagers = await _missionService.GetVoyagers(request);
+            return Ok(voyagers);
+        }
+    }
+}
